ba56c2a075eb61362fa413ea519d1011739100f7
Nikos Maris <nmaris@crypteianetworks.com>
2018-08-21 20:29:41 +0300

    fix missing network host

--- truffle.js
+++ truffle.js
@@ -12,7 +12,14 @@
  *   },
  */
 
+// See <http://truffleframework.com/docs/advanced/configuration>
+// to customize your Truffle configuration!
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
 };
